f4d453235c64f263ce5355b50de6f44c0c94f32d
Matt Gross <mbgross111@gmail.com>
2021-04-28 21:41:01 -0700

    add auto bounce

--- js/men.js
+++ js/men.js
@@ -1,18 +1,28 @@
-$( ".point" ).droppable({
+$(".point").droppable({
   accept: ".man",
   tolerance: "intersect",
   addClasses: false,
   drop: (e, ui) => {
     ui.draggable.attr("style", "position: relative");
     e.target.append(ui.draggable[0]);
+
+    const landedOnGood = $(e.target).children()[0].classList[1] === "good";
+    const isGood = turn;
+    const isAlone = $(e.target).children().length === 2
+    console.log('childrenCount', $(e.target).children().length)
+
+    if (isGood !== landedOnGood && isAlone) {
+      const bounceMe =  $(e.target).children()[0]
+      bounceMe.remove()
+      $("#bounced").append(bounceMe);
+    }
+
+    console.log('turn (true = good)', turn)
   },
-  out: (e, ui) => {
-    console.log(e, ui)
-  }
 });
 
 
-$( ".man" ).draggable({
+$(".man").draggable({
   addClasses: false,
   scroll: false,
   containment: $("#board"),
@@ -20,5 +30,5 @@ $( ".man" ).draggable({
   revert: true,
 });
 
-$( ".man.evil" ).draggable('disable');
-$( ".man.good" ).draggable('disable');
+$(".man.evil").draggable('disable');
+$(".man.good").draggable('disable');
