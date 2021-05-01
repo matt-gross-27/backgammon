9bdc762a7d24097c341abc7fdd71e59dfa380d9d
Matt Gross <mbgross111@gmail.com>
2021-04-30 17:52:41 -0700

    update winning animation, add replay button

--- js/gameWinnerIs.js
+++ js/gameWinnerIs.js
@@ -1,10 +1,8 @@
-let lag = 140;
+let lag = 195;
 let promise = Promise.resolve();
 
-function addWinnerClass(el, str) {
+function addWinnerClass(el) {
   el.remove();
-  let newEl = document.createElement('div');
-
   let newWrapper = document.createElement('div');
   newWrapper.classList = 'winners newWrapper';
 
@@ -12,22 +10,50 @@ function addWinnerClass(el, str) {
 
   el.classList.add('winners');
   newWrapper.append(el);
+}
 
-  el.classList.add('man');
-  el.classList.add(`${str}`);
+function addLosingClass(el, str) {
+  el.remove();
+  document.getElementById("board").append(el);
+  el.classList = (`${str === 'good' ? 'evil' : 'good'} man ui-draggable-handle ui-draggable-disabled losers`);
 }
 
 function gameWinnerIs(str) {
   
+  document.querySelectorAll(".die").forEach(die => {
+    die.remove();
+  });
+  
+  const footerEl = document.querySelector("footer");
+  footerEl.innerHTML = "";
+
+  const playAgain = document.createElement('div');
+  playAgain.setAttribute("title", "Play Again");
+  playAgain.setAttribute("id", "play-again");
+  playAgain.classList = "oi oi-play-circle";
+
+  footerEl.appendChild(playAgain);
+
+  playAgain.addEventListener('click', () => location.reload());
+
   const winners = document.querySelectorAll(`.man.${str}`);
-
-
+  const losers = document.querySelectorAll(`.man.${str === 'good' ? 'evil' : 'good'}`);
+  
   winners.forEach(winningMan => {
     promise = promise.then(()=> {
-      addWinnerClass(winningMan, str);
+      addWinnerClass(winningMan);
       return new Promise((resolve) => {
         setTimeout(resolve, lag)
       });
-    });    
+    });
+  });
+
+  losers.forEach(losingMan => {
+    promise = promise.then(()=> {
+      addLosingClass(losingMan, str);
+      return new Promise((resolve) => {
+        setTimeout(resolve, lag)
+      });
+    });
   });
 };
